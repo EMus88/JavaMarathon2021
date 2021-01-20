1cca3f0e47a6fa017593099b876da0551eca1b83
EMus88 <e.musrepov@gmail.com>
2021-01-20 22:59:09 +0700

    Add Day1 solutions

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -1,7 +1,21 @@
+
 package day1;
+
+import java.util.Scanner;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        System.out.print("Введите цифру от 1 до 9: ");
+        int k=s.nextInt();
+        if (k<1 | k>9) {
+            System.out.println("Вы ввели неверное значение");
+            System.exit(0);
+        }
+        System.out.println("Таблица умножения ждля вашей цифры:");
+        for (int i=1;i<=9;i++){
+            System.out.println(i+"\tx\t"+k+"\t=\t"+i*k);
+        }
     }
 }
+
