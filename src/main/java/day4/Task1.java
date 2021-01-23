1703247e1c1fc38213e8ba484dd61142dcff5189
EMus88 <e.musrepov@gmail.com>
2021-01-23 21:48:48 +0700

    complete day4 tasks

--- src/main/java/day4/Task1.java
+++ src/main/java/day4/Task1.java
@@ -1,7 +1,43 @@
 package day4;
 
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Random rand = new Random();
+        int n=s.nextInt();
+        int more8=0;
+        int equalle1=0;
+        int even=0;
+        int odd=0;
+        int sum=0;
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++){
+            arr[i]= rand.nextInt(11);
+            if (arr[i]>8) {
+                more8++;
+            }
+            if (arr[i]==1){
+                equalle1++;
+            }
+            if (arr[i]%2==0 && arr[i]!=0){
+                even++;
+            }
+            if (arr[i]%2!=0 && arr[i]!=0){
+                odd++;
+            }
+            sum=sum+arr[i];
+        }
+        System.out.println("Сгенерирован следующий массив: " + Arrays.toString(arr));
+        System.out.println("Длина массива: " + arr.length);
+        System.out.println("Количество чисел больше 8: " + more8);
+        System.out.println("Количество чисел равных 1: " + equalle1);
+        System.out.println("Количество четных чисел: " + even);
+        System.out.println("Количество нечетных чисел: " + odd);
+        System.out.println("Сумма всех элементов массива: " + sum);
 
     }
-}
+        }
--- src/main/java/day4/Task3.java
+++ src/main/java/day4/Task3.java
@@ -1,7 +1,27 @@
 package day4;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        Random rand = new Random();
+        int[][] arr = new int[12][8];
+        int[] sum = new int [12];
+        int maxsum=0;
+        int index = 0;
+        for (int i=0; i<12;i++){
+            for (int j=0; j<8;j++){
+                arr[i][j] = rand.nextInt(50);
+                System.out.print("\t" + arr[i][j]);
+                sum[i]=sum[i]+arr[i][j];
+            }
+            System.out.print(" сумма: " + sum[i]);
+            System.out.println();
+            if (sum[i]>=maxsum){
+                maxsum=sum[i];
+                index=i;
+            }
+            }
+        System.out.println("Индекс: " + index);
     }
 }
