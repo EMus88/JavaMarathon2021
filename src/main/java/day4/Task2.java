f6f685aa9c304aa59be190d13c5809f08a20a1c8
EMus88 <e.musrepov@gmail.com>
2021-01-23 21:48:48 +0700

    complete day4 tasks

--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -1,7 +1,31 @@
 package day4;
-
+import java.util.Arrays;
+import java.util.Random;
 public class Task2 {
     public static void main(String[] args) {
-
+        Random rand = new Random();
+        int max = 0;
+        int min=10000;
+        int end0=0;
+        int sum0=0;
+        int[] arr = new int[100];
+        for (int i=0; i<100; i++) {
+            arr[i] = rand.nextInt(10000);
+            if (arr[i]>max){
+                max=arr[i];
+            }
+            if (arr[i]<min){
+                min=arr[i];
+            }
+            if (arr[i]==0 || arr[i]%10==0){
+                end0++;
+                sum0=sum0+arr[i];
+            }
+        }
+        System.out.println("Сгенерирован следующий массив: " + Arrays.toString(arr));
+        System.out.println("наибольший элемент массива :" + max);
+        System.out.println("наименьший элемент массива :" + min);
+        System.out.println("количество элементов массива, оканчивающихся на 0 :" + end0);
+        System.out.println("сумму элементов массива, оканчивающихся на 0 :" + sum0);
     }
 }
--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -1,7 +1,29 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task4 {
     public static void main(String[] args) {
-
+        Random rand = new Random();
+        int[] arr = new int[100];
+        int[] sum = new int[arr.length-2];
+        int maxsum=0;
+        int index=0;
+        for (int i=0; i<100; i++) {
+            arr[i] = rand.nextInt(10000);
+        }
+        for (int i=0; i< arr.length-2;i++){
+            for(int j=i; j<i+3;j++){
+                sum[i]=sum[i]+arr[j];
+            }
+            if (sum[i]>maxsum){
+                maxsum=sum[i];
+                index=i;
+            }
+        }
+        System.out.println("Сгенерирован следующий массив: " + Arrays.toString(arr));
+        System.out.println("Суммы: " + Arrays.toString(sum));
+        System.out.println("Индекс: " + index);
     }
 }
