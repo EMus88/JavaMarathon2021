3eb6e69883d0cbf6cb8712470ed04b1ed0904709
EMus88 <e.musrepov@gmail.com>
2021-01-27 22:30:17 +0700

    complete day 8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,22 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String str = new String();
+        long start1 = System.nanoTime();
+        for (int i = 0; i <= 20000; i++) {
+            str = str + i + " ";
+        }
+        long stop1 = System.nanoTime();
+        long worktime1 = stop1 - start1;
+        System.out.println(worktime1);
 
+        StringBuilder stringBuilder = new StringBuilder();
+        long start2 = System.nanoTime();
+        for (int i = 0; i <= 20000; i++) {
+            stringBuilder.append(i + " ");
+        }
+        long stop2 = System.nanoTime();
+        long worktime2 = stop2 - start2;
+        System.out.println(worktime2);
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,8 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Airplane a320 = new Airplane("Airbus", 1987, 545, 1789);
+        a320.fillUp(250);
+        System.out.println(a320);
     }
 }
